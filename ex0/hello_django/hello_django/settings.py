032ccaf463aee3295d9042c1a0f03dbab13952c5
Beebee1201 <93655718+Beebee1201@users.noreply.github.com>
2026-03-04 20:13:34 +0800

    Update settings.py

--- ex0/hello_django/hello_django/settings.py
+++ ex0/hello_django/hello_django/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-*#)3sth9o2dm4f7(e&who=mpq)e*bvy#3x^t-wa=kjjb8@a-r0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -148,3 +148,4 @@ LOGGING = {
         },
     },
 }
+
