aa315905fb98e759d5d4f75771ac6240f8d49ea2
Beebee1201 <93655718+Beebee1201@users.noreply.github.com>
2026-05-13 14:53:40 +0800

    Add files via upload

---
+++ ex12/react_login/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+import SignInSide from './components/login'; 
+function App() {
+  return (
+    <div className="App">
+
+        <SignInSide />
+
+
+
+    </div>
+  );
+}
+
+export default App;
